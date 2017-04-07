afabaf84f8f90e7834d001534e72d3321ce9c2b1
Cassio Rogerio Eskelsen <eskelsen@gmail.com>
2017-04-07 00:00:04 -0300

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JAVA_OPTS="$JAVA_OPTS -Xmx1536M -XX:MaxPermSize=756M"
+JAVA_OPTS="$JAVA_OPTS -Xmx1536m -Xms1536m -XX:MaxPermSize=756m"
 #JAVA_OPTS="$JAVA_OPTS -Xmx512M -Xms128m -XX:MaxPermSize=128M "
 JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled"
 JAVA_OPTS="$JAVA_OPTS -Duser.timezone=GMT"
